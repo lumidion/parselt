6313120b772a3718bf5c92cd89869c5b68d0017b
David Doyle <d.doyle@cognigy.com>
2022-08-29 17:18:06 +0200

    updating clean script

--- scripts/cleanNonTs.ts
+++ scripts/cleanNonTs.ts
@@ -1,10 +1,13 @@
 import fs from 'fs'
 
-const files = fs.readdirSync('./lib', { withFileTypes: true })
-
-files.forEach((file) => {
-    const shouldFileBeRemoved = file.name.includes('.js') || file.name.includes('.d.ts')
-    if (file.isFile() && shouldFileBeRemoved) {
-        fs.rmSync(`./lib/${file.name}`)
-    }
-})
+const removeNonTsFilesInDir = (dirPath: string) => {
+    const files = fs.readdirSync(dirPath, { withFileTypes: true })
+    files.forEach((file) => {
+        const shouldFileBeRemoved = file.name.includes('.js') || file.name.includes('.d.ts')
+        if (file.isFile() && shouldFileBeRemoved) {
+            fs.rmSync(`${dirPath}/${file.name}`)
+        }
+    })
+}
+removeNonTsFilesInDir('./bin')
+removeNonTsFilesInDir('./lib')
