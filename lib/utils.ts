3b7ca225431bd99be9dd7f0d0c31256b4d84a012
Andrapyre <42009361+Andrapyre@users.noreply.github.com>
2023-02-13 13:24:55 +0100

    fixing formatting issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,12 +2,10 @@ import fs from 'fs'
 import path from 'path'
 import url from 'url'
 
-
-
 export const getPackageVersionOrThrow = (): string => {
     const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
     const packageJsonPath = path.join(__dirname, '..', 'package.json')
-    
+
     const packageJson = fs.readFileSync(packageJsonPath, 'utf8')
     const { version } = JSON.parse(packageJson)
 
--- scripts/updateVersionToTag.ts
+++ scripts/updateVersionToTag.ts
@@ -12,18 +12,16 @@ const updateVersionToTag = () => {
     const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
     const packageJsonPath = path.join(__dirname, '..', 'package.json')
     const packageJson = fs.readFileSync(packageJsonPath, 'utf8')
-    
+
     const packageJsonAsObject = JSON.parse(packageJson)
-    
-    const versionNumber = tagName.replace(/^v/, "")
-    
+
+    const versionNumber = tagName.replace(/^v/, '')
+
     packageJsonAsObject.version = versionNumber
-    
+
     const json = JSON.stringify(packageJsonAsObject, undefined, 4)
-    
+
     fs.writeFileSync(path.join(__dirname, '..', 'package.json'), json)
 }
 
 updateVersionToTag()
-
-
