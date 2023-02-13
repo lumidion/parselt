385d83872421b6bc44150c9d159886953de9a8b5
Andrapyre <42009361+Andrapyre@users.noreply.github.com>
2023-02-13 15:03:38 +0100

    adding logs

---
+++ scripts/initial.sh
@@ -0,0 +1,4 @@
+git tag -d v10.2.0
+git push origin --delete v10.2.0
+git tag v10.2.0 adding-updating-version-script
+git push origin v10.2.0
--- scripts/updateVersionToTag.sh
+++ scripts/updateVersionToTag.sh
@@ -1,3 +1,7 @@
+echo "Start"
 gitref="${GITHUB_REF_NAME}"
+echo $gitref
 version="${gitref:1}"
+echo $version
 npm version $version
+echo "Done"
