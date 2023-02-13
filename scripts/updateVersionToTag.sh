8b5a8a6feaaca5505e4ae2a2294e94dd7c1b36f7
Andrapyre <42009361+Andrapyre@users.noreply.github.com>
2023-02-13 15:15:09 +0100

    more

--- scripts/updateVersionToTag.sh
+++ scripts/updateVersionToTag.sh
@@ -1,7 +1,7 @@
 echo "Start"
 gitref="${GITHUB_REF_NAME}"
 echo $gitref
-version="${gitref:1}"
+version="${GITHUB_REF_NAME:1}"
 echo $version
-npm version $version
+npm version $version --no-git-tag-version
 echo "Done"
